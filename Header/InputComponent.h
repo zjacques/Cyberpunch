3d33c921600b89a5586ef25f62f99d02c5338931
David O'Gorman <davemau51074@gmail.com>
2019-02-05 16:31:55 +0000

    input system created

---
+++ Header/InputComponent.h
@@ -0,0 +1,37 @@
+#ifndef INPUTCOMPONENT_H
+#define INPUTCOMPONENT_H
+
+#include "InputSystem.h"
+#include <utility>
+#include <string>
+#include "SDL.h"
+#include <map>
+typedef std::map<std::string, bool> Input;
+
+class InputComponent  : public Component {
+
+public: 
+	InputComponent()
+	{
+		//Keep a pointer to the keyboard state
+		m_keyStates = SDL_GetKeyboardState(NULL);
+
+		//Add buttons to our input handler
+		m_current["W"] = false;
+		m_current["A"] = false;
+		m_current["S"] = false;
+		m_current["D"] = false;
+		m_current["Space"] = false;
+
+		//Copy the values of current to previous
+		m_previous = m_current;
+	}
+		
+	const Uint8* m_keyStates;
+	Input m_current;
+	Input m_previous;
+};
+
+
+
+#endif
