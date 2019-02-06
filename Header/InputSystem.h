f793981588bb4dcdfe1e1a3eb1bb8d7b088aeedb
Pierce-James <C00203711@itcarlow.ie>
2019-02-06 09:14:20 +0000

    Completed render component and system

--- Header/InputSystem.h
+++ Header/InputSystem.h
@@ -12,14 +12,11 @@ public:
 	~InputSystem() {}
 	void addComponent(Component * component);
 	void update(double dt);
-	void checkKey();
-	void isButtonPressed();
+	void checkKey(std::string m_button, SDL_Keycode code);
+	void isButtonPressed(std::string m_button);
 
 private:
 	InputComponent * m_input;
 };
-
-
-
 
 #endif
