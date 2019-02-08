ff9a51442fdc69ac72ff1351e862ba1f3d9fe4cc
KDaryl <c00207452@itcarlow.ie>
2019-02-08 18:05:55 +0000

    Fixed platform bugs, players are now constant solids rather than sensors

--- Header/CollisionListener.h
+++ Header/CollisionListener.h
@@ -12,6 +12,8 @@ public:
 
 	void BeginContact(b2Contact* contact);
 	void EndContact(b2Contact* contact);
+	void PreSolve(b2Contact* contact, const b2Manifold* oldManifold);
+	void PostSolve(b2Contact* contact, const b2ContactImpulse* impulse);
 	void flipGravity();
 private:
 	bool m_gravFlipped;
